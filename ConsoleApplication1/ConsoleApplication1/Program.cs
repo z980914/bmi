7c0f6082184606d24a3b5682bd961c44d53004e7
m305-61\w401_ <1604314896@qq.com>
2018-03-15 17:49:03 +0800

    shuai

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("===========================");
+            Console.WriteLine("!                         !");
+            Console.WriteLine("!                         !");
+            Console.WriteLine("!            DNA          !");
+            Console.WriteLine("!                         !");
+            Console.WriteLine("!                         !");
+            Console.WriteLine("===========================");
+            Console.ReadLine();
+            System.Threading.Thread.Sleep(1000);
+            Console.ReadLine();
+         
+            
+        }
+    }
+}
